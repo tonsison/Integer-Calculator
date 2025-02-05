07fd3b1692341d34a628ba536059cafb7de306f6
tonsison <tonitosison@gmail.com>
2025-02-05 18:22:56 +0800

    Added exit command

--- main.cpp
+++ main.cpp
@@ -2,6 +2,19 @@
 
 using namespace std;
 
+// Create Addition Function
+
+// Create Multiplication Function
+
 int main()
 {
+  string userInput;
+
+  cout << "";
+  cin >> userInput;
+
+  if (userInput == "exit")
+  {
+    return 0;
+  }
 }
